932120d9928b66b528d7d6c3dc1b5cece10abd80
Morgan Bonar <mbonar@gmail.com>
2017-03-13 23:23:06 -0700

    First commit

---
+++ __tests__/welcome.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import expect from 'expect';
+import { shallow } from 'enzyme';
+import Welcome from '../src/components/Welcome.js';
+
+describe('Welcome (Snapshot)', () => {
+    it('Welcome renders hello world', () => {
+        const welcome = shallow(<Welcome />);
+        expect(welcome.find('div').text()).toEqual('Hello World');
+    });
+});
