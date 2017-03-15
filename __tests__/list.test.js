eafa2b3eb5e2aa5a78df588df35322883942d6e6
morgan.bonar <morgan.bonar@nike.com>
2017-03-14 20:03:03 -0700

    more

--- __tests__/list.test.js
+++ __tests__/list.test.js
@@ -14,4 +14,14 @@ describe('List', () => {
     it('Class of rendered table', () => {
         expect(list.find('.myClass').length).toEqual(1);
     });
+    it('List renders a column', () => {
+        const arr = list.find('th');
+        expect(arr.length).toEqual(1);
+        expect(arr.first().text()).toEqual('Name');
+    });
+    it('List renders data', () => {
+        const arr = list.find('td');
+        expect(arr.length).toEqual(3);
+        expect(arr.at(1).text()).toEqual('Name 2');
+    });
 });
